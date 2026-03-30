ff1043ed823636f180248b893077c98fedb81b11
Dylan MacKenzie <ecstaticmorse@gmail.com>
2026-03-29 20:17:02 -0700

    fixup: int64

--- lite/demo/cxx/lac_demo/lac.cc
+++ lite/demo/cxx/lac_demo/lac.cc
@@ -61,7 +61,7 @@ int LAC::feed_data(const std::vector<std::string> &querys) {
     shape += this->_seq_words.size();
     this->_lod[0].push_back(shape);
   }
-  this->_input_tensor->Resize({shape, 1});
+  this->_input_tensor->Resize({static_cast<int64_t>(shape), 1});
   this->_input_tensor->SetLoD(this->_lod);
   int64_t *input_d = this->_input_tensor->mutable_data<int64_t>();
   int index = 0;
